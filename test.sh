28b69689ba6b1b75328c9714f0515d45e1f99734
simonhuang <simonhuang@truetel.com>
2015-12-04 10:26:06 +0800

    add memo

--- test.sh
+++ test.sh
@@ -5,3 +5,38 @@ echo "YmdHMS 	$(date +"%Y_%m_%d_%H_%M_%S")"
 
 echo $basePath
 echo $parts
+
+echo
+
+cd $basePath/ClientApps
+
+pwd
+files=$(ls $basePath/ClientApps)
+
+#echo $files
+
+latesttime=0
+latestfile=0
+time=0
+filetime=0
+
+for file in $files
+do		
+	printf "[ $file ]\n" 
+	printf ">> `stat -c %Y $file`\n"
+	printf ">> `stat -c %y $file`\n"
+	filetime=`stat -c %Y $file`
+	if [ $filetime>$time ]; then
+		time=$filetime
+		latesttime=`stat -c %y $file`
+		latestfile=$file
+		
+
+	printf "$time	$latesttime	$latestfile"
+	echo
+
+	fi
+done
+
+echo
+printf "latest \nfile: $latestfile \ntime: $latesttime"
