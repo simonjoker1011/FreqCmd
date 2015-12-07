df91e6dcf63ad7ed6b401ccdf4f1ee2ee12ad71c
simonhuang <simonhuang@truetel.com>
2015-12-07 13:08:52 +0800

    add backup

--- parts/backup.sh
+++ parts/backup.sh
@@ -1,4 +1,13 @@
-#! bin/bash
+#! /bin/bash
+
+function press2continue(){
+
+        echo    
+        echo "press return button to continue."
+        read -s
+
+}
+
 
 # ===========================================
 # 先輸入系統所需要的資料
@@ -6,20 +15,24 @@ host="192.168.1.116"		# 遠端主機
 id="simonhuang"			# 遠端主機的 FTP 帳號
 pw='simon!#!& '		# 該帳號的密碼
 
-basedir="/backup/weekly"	# 本地端的欲被備份的目錄
-remotedir="/home/backup"	# 備份到遠端的何處？
+basedir[0]="/etc/"	# 本地端的欲被備份的目錄
+basedir[1]="/home/"
+basedir[2]="/var/spool/mail"
+basedir[3]="/var/spool/cron"
+basedir[4]="/root"
+
+remotedir="~/remote.bak"	# 備份到遠端的何處？
 
 # ===========================================
 
-backupfile=weekly.tar.bz2
+for dir in ${basedir[@]}
+do
+	echo ">> backup $dir"
+	echo
 
-cd $basedir/..
-  tar -jpc -f $backupfile $(basename $basedir)
+	rsync -av -e ssh $dir ${id}@${host}:${remotedir}$dir | grep rsync
 
-ftp -n "$host" > ${basedir}/../ftp.log 2>&1 <<EOF
-user $id $pw
-binary
-cd $remotedir
-put $backupfile
-bye
-EOF
+done
+
+
+
